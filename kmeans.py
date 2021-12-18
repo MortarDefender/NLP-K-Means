ab19246566cee3149102db30dcf29a14385674a0
MortarDefender <63960808+MortarDefender@users.noreply.github.com>
2021-12-18 22:01:06 +0200

    kmeans and kmeans plus inital functions declerations

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,21 @@
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Kmeans:
-    def __init__(self):
+    def __init__(self, numberOfClusters = 0):
+        self.__numberOfClusters = numberOfClusters
+        self__maxNumberOfIterations = 100
+
+    def __placeClusterCentroids(self):
+        pass
+
+    def __findNearstCentrois(self):
+        pass
+
+    def plot(self):
+        pass
+    
+    def fit(self):
         pass
--- kmeansPlus.py
+++ kmeansPlus.py
@@ -1,6 +1,20 @@
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class KmeansPlus:
     def __init__(self):
+        pass
+
+    def __placeClusterCentroids(self):
+        pass
+    
+    def __computeDistance(self):
+        pass
+    
+    def plot(self):
+        pass
+    
+    def fit(self):
         pass
