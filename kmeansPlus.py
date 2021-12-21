3a0d6e1ed6eaf3397b736658469dba5721bf247b
roiflint <roiflint@hotmail.com>
2021-12-21 17:06:46 +0200

    Added groups

--- kmeansPlus.py
+++ kmeansPlus.py
@@ -10,7 +10,7 @@ class KmeansPlus:
         self.__K = numOfClusters
         self.__vectors = getFeatureVectors(fileName)
         self.__centroids = [0 for i in range(self.__K)]
-        self.__distances = [1 for i in enumerate(self.__vectors)]
+        self.__distances = [[1,0] for i in enumerate(self.__vectors)]
         self.__weightedDistances = [1 for i in enumerate(self.__vectors)]
 
     def __placeClusterCentroids(self):
@@ -25,13 +25,14 @@ class KmeansPlus:
     def fit(self):
         self.__centroids[0] = self.__placeClusterCentroids(self)
         for i, point in enumerate(self.__vectors):
-            self.__distances[i] = self.__computeDistance(point[0], self.__centroids[0])
-            self.__weightedDistances[i] = pow(self.__distances[i],2)
+            self.__distances[i][0] = self.__computeDistance(point[0], self.__centroids[0])
+            self.__weightedDistances[i] = pow(self.__distances[i][0],2)
 
         for i in range(1,self.__K):
             self.__centroids[i] = self.__placeClusterCentroids(self)
             for j, point in enumerate(self.__vectors):
                 D = self.__computeDistance(point[0],self.__centroids[i])
                 if D < self.__distances[j]:
-                    self.__distances[j] = D
+                    self.__distances[j][1] = i
+                    self.__distances[j][0] = D
                     self.__weightedDistances[j] = pow(D,2)
